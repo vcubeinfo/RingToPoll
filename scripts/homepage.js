bfdaa2706cc2aafc57f59d3ac5177fae0e6d21a0
Vamsi Krishna Varma <vamsi.gunturi@gmail.com>
2014-09-23 16:04:47 +0530

    Data Table Plug-in for data display Added
    http://www.codeproject.com/Articles/194916/Enhancing-HTML-tables-using-a
    -JQuery-DataTables-pl

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -4,5 +4,136 @@ $(function(){
     var pollData = $.getJSON("localJson/pollJsonData.json", function(response){
         donutchart('.pieChartView', response.data);
 		renderBarCharts('.barChartView', response.data);
-    })
-})
+    });
+    
+    $('#tableGrid').dataTable({
+    				"paging": true,
+    	             "pagingType": "full_numbers", 
+    				 "aoColumns": [
+        				{ "mDataProp": "public" },
+				        { "mDataProp": "private" }
+				      ],	
+					  "aaData": [
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    },
+					    {
+					      "public": "123456",
+					      "private": "987654"
+					    }
+					] 
+				});
+});
