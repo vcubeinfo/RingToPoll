24e7ef2c67da5770dd8cd1705345b8603c1f1260
Thirupal <thirupal@divami.com>
2014-09-23 14:32:29 +0530

    chart section done

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,8 @@
 /** This is the home page file **/
 
 $(function(){
-
+    var pollData = $.getJSON("localJson/pollJsonData.json", function(response){
+        donutchart('.pieChartView', response.data);
+		renderBarCharts('.barChartView', response.data);
+    })
 })
