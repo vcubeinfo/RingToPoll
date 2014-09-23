0b56079ada27b764fd957bb8fce7f737e4b372cb
Thirupal <thirupal@divami.com>
2014-09-23 18:45:22 +0530

    UI style changes

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,149 +1,37 @@
-/** This is the home page file **/
-
+/** 
+ * This is the home page file -
+ * Basically, In this file we will take the data from local json and use that to chart views and table views
+ * This is the controller of this page
+ */
 $(function(){
     var pollData = $.getJSON("localJson/pollJsonData.json", function(response){
-        donutchart('.pieChartView', response.data);
-		renderBarCharts('.barChartView', response.data);
+		var responseData = response.data;
+		
+		//Dynamic title for chart views
+		$(".chartTitle").text(responseData.name);
+		
+		//This is for pie chart
+        donutchart('.pieChartView', responseData);
+		
+		//This is for column chart
+		renderBarCharts('.barChartView', responseData);
+		
+		//This is for grid data
+        $('#tableGrid').dataTable(responseData.gridData);
     });
     
+	//From date picker
    	$('.fromDatepicker').datepicker({
    		//'buttonImage': ''
    	}).on('change', function(e) {
 		this.value;
 	});
+	
+	//To date picker
 	$('.toDatepicker').datepicker({
    		//'buttonImage': ''
    	}).on('change', function(en) {
 		this.value;
 	});
     
-    $('#tableGrid').dataTable({
-    				 "sPaginationType": "full_numbers",
-    				 "aoColumns": [
-        				{ "mDataProp": "public" },
-				        { "mDataProp": "private" }
-				      ],	
-					  "aaData": [
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    },
-					    {
-					      "public": "123456",
-					      "private": "987654"
-					    }
-					] 
-				});
 });
